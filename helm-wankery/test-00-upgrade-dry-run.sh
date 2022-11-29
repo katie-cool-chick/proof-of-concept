600e5be07435207b5ce0b3dbf3b20398454ccfc8
Katie Dukes <katie.trivia.chick@gmail.com>
2022-11-29 13:59:24 -0500

    And non-existent apps/v2 api fails successfully

--- helm-wankery/test-00-upgrade-dry-run.sh
+++ helm-wankery/test-00-upgrade-dry-run.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 set -o pipefail
 name="${0##*/}"
 export HELM_NAMESPACE="$name"
 cleanup() {
-	set +e
 	helm uninstall wankery; echo $?
 	helm uninstall wankery-broken-api-1; echo $?
 	helm uninstall wankery-broken-api-2; echo $?
 	helm uninstall wankery-deprecated-api; echo $?
 }
 cleanup
-set -e
+set -ex
 helm upgrade --install --create-namespace --dry-run wankery ./wankery; echo $?
+! helm upgrade --install --create-namespace --dry-run wankery-broken-api-1 ./wankery-broken-api-1; echo $?
+set +ex
 cleanup
